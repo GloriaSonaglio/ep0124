0d1766e0fdaaa081c30c1977708cdb67e51acaa0
GloriaSonaglio <128263565+GloriaSonaglio@users.noreply.github.com>
2024-03-11 12:13:52 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+const botoes = document.querySelectorAll("botao")
+
+for(let i=; i<botoes.length;i++){
+    botoes[i].inclick = fuction{
+        for(let j=0; j<botoes.length; j++){
+            .,kljkk
+        }
+        botoes[i].classList.add("acionado");
+    }
+    botoes[i].classList.add("acionado");
+    
